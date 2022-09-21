9322ffabd6a2ff79817292bed46e6326509da454
unknown <harisali2928@gmail.com>
2022-09-21 02:21:01 -0700

    ecommerece store

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
+import { StateContext } from "../context/stateContext";
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}
+
+export default MyApp;
